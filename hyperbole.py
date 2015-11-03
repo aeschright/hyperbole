ef150af6095d7a5904532a858b58b35924f9d03b
Audrey Eschright <spinnerin@gmail.com>
2015-11-03 13:49:28 -0800

    Splitting the three source works into chapters and outputting the first chapter of each work into a single “novel”.

--- hyperbole.py
+++ hyperbole.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 
-baskervilles = open('sources/pg2852.txt')
-pride = open('sources/pg1342.txt')
-dunwich = open('sources/pg50133.txt')
+import re
 
-a_new_novel = str(baskervilles.read()) + str(pride.read()) + str(dunwich.read())
+baskervilles = open('sources/pg2852.txt')
+baskervilles_chapters = re.split("\nChapter [0-9]+\.\s[a-zA-Z.\s]+\n", str(baskervilles.read()))
+
+pride = open('sources/pg1342.txt')
+pride_chapters = re.split("\nChapter [0-9]+\s*\n", str(pride.read()))
+
+dunwich = open('sources/pg50133.txt')
+dunwich_chapters = re.split("\n[0-9]+\n", str(dunwich.read()))
+
+a_new_novel = baskervilles_chapters[1] + pride_chapters[1] + dunwich_chapters[1]
 
 with open('novel.txt', 'w') as f:
 	f.write(a_new_novel)
