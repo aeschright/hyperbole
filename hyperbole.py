5984d8d679a4793f104d89d27c5d80a3f6f8d514
Audrey Eschright <spinnerin@gmail.com>
2015-11-03 13:06:14 -0800

    Minor refactoring

--- hyperbole.py
+++ hyperbole.py
@@ -4,7 +4,7 @@ baskervilles = open('sources/pg2852.txt')
 pride = open('sources/pg1342.txt')
 dunwich = open('sources/pg50133.txt')
 
+a_new_novel = str(baskervilles.read()) + str(pride.read()) + str(dunwich.read())
+
 with open('novel.txt', 'w') as f:
-	f.write(baskervilles.read())
-	f.write(pride.read())
-	f.write(dunwich.read())
+	f.write(a_new_novel)
