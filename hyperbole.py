b1d3410d260bf381612249888e9d4a49400886c1
Audrey Eschright <spinnerin@gmail.com>
2015-11-03 14:30:43 -0800

    Assembling a 12 chapter novel by: picking a random first chapter, ten assorted other chapters from the sources, and a random last chapter.
    Word count: 24161

--- hyperbole.py
+++ hyperbole.py
@@ -1,17 +1,36 @@
 #!/usr/bin/env python3
 
+import random
 import re
 
 baskervilles = open('sources/pg2852.txt')
-baskervilles_chapters = re.split("\nChapter [0-9]+\.\s[a-zA-Z.\s]+\n", str(baskervilles.read()))
+baskervilles_chapters = re.split("\n\nChapter [0-9]+\.\s[a-zA-Z .]+\n\n", str(baskervilles.read()))
+baskervilles_chapters.pop(0) # removing everything before the start of the first chapter
 
 pride = open('sources/pg1342.txt')
-pride_chapters = re.split("\nChapter [0-9]+\s*\n", str(pride.read()))
+pride_chapters = re.split("\n\nChapter [0-9]+\s*\n\n", str(pride.read()))
+pride_chapters.pop(0) # removing everything before the start of the first chapter
 
 dunwich = open('sources/pg50133.txt')
-dunwich_chapters = re.split("\n[0-9]+\n", str(dunwich.read()))
+dunwich_chapters = re.split("\n\n[0-9]+\n\n", str(dunwich.read()))
+dunwich_chapters.pop(0) # removing everything before the start of the first chapter
 
-a_new_novel = baskervilles_chapters[1] + pride_chapters[1] + dunwich_chapters[1]
+source_chapters = [baskervilles_chapters, pride_chapters, dunwich_chapters]
+
+a_new_novel = "Chapter 1\n\n"
+a_new_novel += random.choice(source_chapters)[0]
+
+# randomly pick 10 chapters
+for i in range(10):
+	chapter_number = str(i+2)
+	chapter_title = "Chapter " + chapter_number + "\n\n"
+	selected_source = random.choice(source_chapters)
+	chapter_index = random.randint(1, len(selected_source) - 2)
+	chapter = selected_source[chapter_index]
+	a_new_novel += chapter_title + chapter + "\n\n"
+
+a_new_novel += "Chapter 12\n\n"
+a_new_novel += random.choice(source_chapters)[-1]
 
 with open('novel.txt', 'w') as f:
 	f.write(a_new_novel)
